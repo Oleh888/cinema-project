2eaadfa9a01a8d4de1cb58e0b0c0693475c6ed11
Oleh Kuzmenko <oleg24453@gmail.com>
2020-05-27 14:43:57 +0300

    created dao, service and model for Order added method clear to the ShoppingCartService

---
+++ src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,61 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.OrderDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.apache.log4j.Logger;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    private static final Logger LOGGER = Logger.getLogger(OrderDaoImpl.class);
+
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            LOGGER.info(String.format("Order with id: %d was added to the DB.", order.getId()));
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert ticket entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public List<Order> getOrderHistory(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Order> criteriaQuery =
+                    criteriaBuilder.createQuery(Order.class);
+            Root<Order> root = criteriaQuery.from(Order.class);
+            Predicate predicateForOrder = criteriaBuilder.equal(root.get("user"), user);
+            criteriaQuery.where(predicateForOrder);
+            List<Order> orders = session.createQuery(criteriaQuery).getResultList();
+            orders.forEach(o -> Hibernate.initialize(o.getTickets()));
+            return orders;
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get shopping cart by user: " + user, e);
+        }
+    }
+}
