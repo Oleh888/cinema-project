09845157938b99acb020613a515c1172b193ffe9
Oleh Kuzmenko <oleg24453@gmail.com>
2020-06-11 12:43:02 +0300

    added CinemaHallController with DTO

---
+++ src/main/java/com/dev/cinema/controller/CinemaHallController.java
@@ -0,0 +1,45 @@
+package com.dev.cinema.controller;
+
+import com.dev.cinema.model.CinemaHall;
+import com.dev.cinema.model.dto.CinemaHallRequestDto;
+import com.dev.cinema.model.dto.CinemaHallResponseDto;
+import com.dev.cinema.service.CinemaHallService;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cinema-halls")
+public class CinemaHallController {
+    private final CinemaHallService cinemaHallService;
+
+    public CinemaHallController(CinemaHallService cinemaHallService) {
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    @PostMapping(value = "/add")
+    public CinemaHall addCinemaHall(@RequestBody CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        return cinemaHallService.add(cinemaHall);
+    }
+
+    @GetMapping(value = "/all")
+    public List<CinemaHallResponseDto> getAll() {
+        return cinemaHallService.getAll().stream()
+                .map(this::getCinemaHallResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    private CinemaHallResponseDto getCinemaHallResponseDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
+        cinemaHallResponseDto.setCapacity(cinemaHall.getCapacity());
+        cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
+        return cinemaHallResponseDto;
+    }
+}
