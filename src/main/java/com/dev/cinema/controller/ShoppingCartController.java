4e5675999038639762ec7ea428197631fe38de5b
Oleh Kuzmenko <oleg24453@gmail.com>
2020-06-14 23:03:30 +0300

    Configured Spring Security with in memory authentication Replaced all ?userId @RequestParam in your code with Authentication object

--- src/main/java/com/dev/cinema/controller/ShoppingCartController.java
+++ src/main/java/com/dev/cinema/controller/ShoppingCartController.java
@@ -1,8 +1,11 @@
 package com.dev.cinema.controller;
 
+import com.dev.cinema.model.User;
 import com.dev.cinema.model.dto.TicketResponseDto;
 import com.dev.cinema.model.mapper.ShoppingCartMapper;
+import com.dev.cinema.service.UserService;
 import java.util.List;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/shopping-carts")
 public class ShoppingCartController {
     private final ShoppingCartMapper shoppingCartMapper;
+    private final UserService userService;
 
-    public ShoppingCartController(ShoppingCartMapper shoppingCartMapper) {
+    public ShoppingCartController(ShoppingCartMapper shoppingCartMapper, UserService userService) {
         this.shoppingCartMapper = shoppingCartMapper;
+        this.userService = userService;
     }
 
     @PostMapping(value = "/add-movie-session")
@@ -25,7 +30,8 @@ public class ShoppingCartController {
     }
 
     @GetMapping(value = "/by-user")
-    public List<TicketResponseDto> getByUser(@RequestParam Long userId) {
-        return shoppingCartMapper.getTicketResponseDtoByUserId(userId);
+    public List<TicketResponseDto> getByUser(Authentication authentication) {
+        User user = userService.findByEmail(authentication.getName()).get();
+        return shoppingCartMapper.getTicketResponseDtoByUserId(user.getId());
     }
 }
